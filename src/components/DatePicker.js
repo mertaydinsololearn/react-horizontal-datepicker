8090ee498f54bd2c246376d35631e81f8b1c3ec1
mertaydin99 <mertaydin991@hotmail.com>
2023-08-26 02:13:15 +0300

    .

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,15 +1,25 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, {useEffect, useState} from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
 import { addDays, subDays } from "date-fns";
-import React from "react";
 import hexToRgb from "../global/helpers/hexToRgb";
 import styles from "./DatePicker.module.css"
 import { DateView } from "./DateView";
 import { MonthView } from './MonthView';
+import styles from "./DatePicker.module.css"
+import {
+    addDays,
+    addMonths,
+    differenceInMonths,
+    format,
+    isSameDay,
+    lastDayOfMonth,
+    startOfMonth
+} from "date-fns";
 
-const DatePicker = (props) => {
-	const [date, setDate] = useState(props.selectDate.toDateString());
-    const next = (event) => {
+
+const DatePicker = ({startDate, lastDate, prevDate, locale, selectDate, getSelectedDay, labelFormat, marked}) => {
+  const next = (event) => {
         event.preventDefault();
         const e = document.getElementById('container');
         const width = e ? e.getBoundingClientRect().width : null;
@@ -32,25 +42,188 @@ const DatePicker = (props) => {
 
     let buttonzIndex = {zIndex: 2};
     let buttonStyle = {background: primaryColor};
-    let Component = DateView;
+    
+    const [selectedDate, setSelectedDate] = useState(null);
+    const firstSection = {marginLeft: '40px'};
+    const selectedStyle = {fontWeight:"bold",width:"45px",height:"45px",borderRadius:"50%",border:`2px solid ${primaryColor}`,color:primaryColor};
+    const labelColor = {color: primaryColor};
+    const markedStyle = {color: "#8c3737", padding: "2px", fontSize: 12};
 
-    if (props.type === "month") {
-        buttonzIndex = {zIndex: 5};
-        Component = MonthView;
-        buttonStyle = {background: primaryColor, marginBottom: "5px"};
+    const getStyles = (day) => {
+        return isSameDay(day, selectedDate)?selectedStyle:null;
+    };
+
+    const getId = (day) => {
+        return isSameDay(day, selectedDate)?'selected':"";
+    };
+
+    const getMarked = (day) => {
+        let markedRes = marked?.find(i => isSameDay(i.date, day));
+        if (markedRes) {
+            if (!markedRes?.marked) {
+                return;
+            }
+
+            return <div style={{ ...markedRes?.style ?? markedStyle }} className={styles.markedLabel}>
+                {markedRes.text}
+            </div>;
+        }
+
+        return "";
+    };
+
+    const renderDays = () => {
+        const dayFormat = "E";
+        const dateFormat = "d";
+
+        const months = [];
+        let days = [];
+
+
+    const numberOfMonths = eachMonthOfInterval({
+        start: prevDate,
+        end: startDate
+      });
+      
+
+        // const styleItemMarked = marked ? styles.dateDayItemMarked : styles.dateDayItem;
+        for (let i = 0; i < numberOfMonths.length; i++) {
+            let start, end;
+            const month = startOfMonth(addMonths(prevDate, i));
+
+            start = i === 0 ? Number(format(prevDate, dateFormat, {locale: locale})) - 1 : 0;
+            end = i ===  numberOfMonths.length - 1 ? Number(format(subDays(startDate, 1), "d")) : Number(format(lastDayOfMonth(month), "d"));
+
+            for (let j = start; j < end; j++) {
+                let currentDay = addDays(month, j);
+
+                days.push(
+                    <div id={`${getId(currentDay)}`}
+                         className={marked ? styles.dateDayItemMarked : styles.dateDayItem}
+                         style={getStyles(currentDay)}
+                         key={currentDay.toString() + Math.random()}
+                         onClick={() => onDateClick(currentDay)}
+                    >
+                        <div className={styles.dayLabel}>{format(currentDay, dayFormat, {locale: locale})}</div>
+                        <div className={styles.dateLabel}>{format(currentDay, dateFormat, {locale: locale})}</div>
+                        {getMarked(currentDay)}
+                    </div>
+                );
+            }
+            months.push(
+                <div className={styles.monthContainer}
+                     key={month.toString() + Math.random()}
+                >
+                    <span className={styles.monthYearLabel} style={labelColor}>
+                        {format(month, labelFormat || "MMMM yyyy", {locale: locale})}
+                    </span>
+                    <div className={styles.daysContainer} style={i===0?firstSection:null}>
+                        {days}
+                    </div>
+                </div>
+            );
+            days = [];
+
+        }
+
+        
+
+        for (let i = 0; i <= differenceInMonths(lastDate, startDate); i++) {
+            let start, end;
+            const month = startOfMonth(addMonths(startDate, i));
+
+            start = i === 0 ? Number(format(startDate, dateFormat, {locale: locale})) - 1 : 0;
+            end = i === differenceInMonths(lastDate, startDate) ? Number(format(lastDate, "d", {locale:locale})) : Number(format(lastDayOfMonth(month), "d", {locale: locale}));
+
+            for (let j = start; j < end; j++) {
+                let currentDay = addDays(month, j);
+
+                days.push(
+                    <div id={`${getId(currentDay)}`}
+                         className={marked ? styles.dateDayItemMarked : styles.dateDayItem}
+                         style={getStyles(currentDay)}
+                         key={currentDay.toString() + Math.random()}
+                         onClick={() => onDateClick(currentDay)}
+                    >
+                        <div className={styles.dayLabel}>{format(currentDay, dayFormat, {locale: locale})}</div>
+                        <div className={styles.dateLabel}>{format(currentDay, dateFormat, {locale: locale})}</div>
+                        {getMarked(currentDay)}
+                    </div>
+                );
+            }
+            months.push(
+                <div className={styles.monthContainer}
+                     key={month.toString() + Math.random()}
+                >
+                    <span className={styles.monthYearLabel} style={labelColor}>
+                        {format(month, labelFormat || "MMMM yyyy", {locale: locale})}
+                    </span>
+                    <div className={styles.daysContainer} style={i===0?firstSection:null}>
+                        {days}
+                    </div>
+                </div>
+            );
+            days = [];
+
+        }
+
+        return <div id={"container"} className={styles.dateListScrollable}>{months}</div>;
     }
 
-    return (
-        <div className={styles.container}>
+    const onDateClick = day => {
+        setSelectedDate(day);
+        if (getSelectedDay) {
+            getSelectedDay(day);
+        }
+    };
+
+    useEffect(() => {
+        if (getSelectedDay) {
+            if (selectDate) {
+                getSelectedDay(selectDate);
+            } else {
+                getSelectedDay(startDate);
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        if (selectDate) {
+            if (!isSameDay(selectedDate, selectDate)) {
+                setSelectedDate(selectDate);
+                setTimeout(() => {
+                    let view = document.getElementById('selected');
+                    if (view) {
+                        view.scrollIntoView({behavior: "smooth", inline: "center", block: "nearest"});
+                    }
+                }, 20);
+            }
+        }
+    }, [selectDate]);
+        useEffect(() => {
+        if (selectedDate) {
+                    let view = document.getElementById('selected');
+                    if (view) {
+                        view.scrollIntoView({behavior: "smooth", inline: "center", block: "nearest"});
+                    }
+                };
+    }, [selectedDate]);
+
+    return(
+      <div className={styles.container}>
             <div className={styles.buttonWrapper} style={buttonzIndex}>
                 <button className={styles.button} style={buttonStyle} onClick={prev}>&lt;</button>
             </div>
-            <Component {...props} primaryColor={primaryColor} startDate={startDate} lastDate={lastDate} prevDate={prevDate} locale={props.locale} date={date}/>
+    			 <React.Fragment>{renderDays()}</React.Fragment>
             <div className={styles.buttonWrapper} style={buttonzIndex}>
                 <button className={styles.button} style={buttonStyle} onClick={next}>&gt;</button>
             </div>
         </div>
-    )
+    );
+
 }
+
+
+
 
 export { DatePicker }
